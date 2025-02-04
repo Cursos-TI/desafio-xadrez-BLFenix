0dc1038eb50e1f2735d7389a3ff23137b525aecb
BLFenix <gabrielramoscastro761@gmail.com>
2025-02-04 08:48:31 -0300

    Criando a função numPeca

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,55 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+int selecionarPeca()
+{
+    int numPeca;
+    do
+    {
+        printf("\nAnalise a lista abaixo:");
+        printf("\n1 - Bispo:");
+        printf("\n2 - Torre:");
+        printf("\n3 - Rainha:");
+
+        printf("Digite a peça que deseja mover:");
+        scanf(" %d", &numPeca);
+
+        if (numPeca != 1 && numPeca != 2 && numPeca != 3)
+        {
+            printf("Por favor, selecione a peça de 1 a 3");
+            continue;
+        }
+
+    } while (numPeca != 1 && numPeca != 2 && numPeca != 3);
+
+    return numPeca;
+}
+
+int main()
+{
+    const char direita = 'direita';
+    const char esquerda = 'esquerda';
+    const char cima = 'cima';
+    const char baixo = 'baixo';
+    int numPeca = selecionarPeca();
+
+    switch (numPeca)
+    {
+    case 1: // Bispo
+
+        break;
+    case 2: // Torre
+
+        break;
+    case 3: // Rainha
+
+        break;
+
+    default:
+        printf("\n\nERRO NO CÓDIGO!\n\n");
+        break;
+    }
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
